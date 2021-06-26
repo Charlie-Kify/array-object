827fb590e22744a55a1d4102c0a8a4977101eb18
Kify <19012564@es.uveg.edu.mx>
2021-06-26 18:00:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from './Interfaces/user/user.intrface'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'array-object';
+export class AppComponent implements OnInit {
+  users: IUser[] = [];
+
+  constructor(){}
+
+  ngOnInit(){
+    this.users = [
+      {
+        name: 'Juan',
+        lastname: 'Perez',
+        age: 34
+      },
+      {
+        name: 'Luis',
+        lastname: 'Fernandez',
+        age: 35
+      },
+      {
+        name: 'Ariely',
+        lastname: 'Torrres',
+        age: 45
+      }
+    ]
+  }
+
+  onDeleteUser(index:number){
+    this.users.splice(index,1);
+  }
+  
 }
